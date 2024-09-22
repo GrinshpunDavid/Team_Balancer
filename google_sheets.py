c431344c07f19d3cdb4ede682a6de609d11f6a56
David Grinshpun <grinshpundavid@gmail.com>
2024-09-22 09:54:03 +0300

    remove data

--- google_sheets.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-
-def google_sheets_to_df(sheet_url: str) -> pd.DataFrame:
-    sheet_id = sheet_url.split('/d/')[1].split('/')[0]
-    csv_url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv'
-    return pd.read_csv(csv_url)
